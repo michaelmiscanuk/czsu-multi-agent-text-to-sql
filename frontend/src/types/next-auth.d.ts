621bf82f09b9015e70f09a4ba4786d3ffd3c329f
michaelmiscanuk <retko85@gmail.com>
2025-06-03 13:16:36 +0200

    41d Auth fix

--- frontend/src/types/next-auth.d.ts
+++ frontend/src/types/next-auth.d.ts
@@ -1,7 +1,14 @@
 import NextAuth from "next-auth";
+import { JWT } from "next-auth/jwt";
 
 declare module "next-auth" {
   interface Session {
+    id_token?: string;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
     id_token?: string;
   }
 } 
