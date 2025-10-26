9d30c48a74df51374668d9b61c24073527b31d62
michaelmiscanuk <retko85@gmail.com>
2025-10-26 19:27:05 +0100

    249 Numbering sequence of script to get czsu data into sqllite cloud (for updates). For now, manual upload of local sqlite to sqlite cloud

---
+++ data/upload_czsu_data_db_to_sqlite_cloud_03.py
@@ -0,0 +1,5 @@
+# manually via UI upload to cloud sqlite db
+# https://dashboard.sqlitecloud.io/
+# go to your project, choose "Upload Database" and drag czsu_data.db file there
+
+# TODO: automate via API if available
