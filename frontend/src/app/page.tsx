48d0cf2242969e6ed8c7c5b738c5ffdabca8a555
Michael Miscanuk <retko85@gmail.com>
2026-01-08 23:44:56 +0100

    363 Change to CZSO in UI

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="w-full max-w-5xl bg-white flex flex-col rounded-2xl shadow-2xl border border-gray-100 overflow-hidden min-h-[70vh] p-8">
       <div className="flex flex-1 flex-col items-center justify-center text-center p-12">
-        <h1 className="text-3xl font-bold mb-4">Welcome to the CZSU Data Explorer and Chatbot</h1>
+        <h1 className="text-3xl font-bold mb-4">Welcome to the CZSO Data Explorer and Chatbot</h1>
         <p className="text-xl text-gray-700 max-w-2xl">
           This application contains data from the{' '}
           <a 
